5c986480eeaf5a8765470ecae4ffeaa5ae0a1f0a
cpalaka <cpalaka@gmail.com>
2020-01-21 22:29:46 -0700

    add theme + global styles boilerplate

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
+import GlobalStyles from './src/theme/globalStyles'
+
+export const wrapRootElement = ({ element }) =>
+    <ThemeProvider theme={theme}>
+        <>
+            {element}
+            <GlobalStyles />
+        </>
+    </ThemeProvider>
---
+++ src/theme/index.js
@@ -0,0 +1,3 @@
+export default {
+    //add theme constants here
+}
