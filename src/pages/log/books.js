6ecb35625739c57d563456fc41180937747135be
Chaitanya Palaka <cpalaka@gmail.com>
2020-04-24 18:37:48 -0700

    /log/books call to goodreads api

--- src/pages/log/books.js
+++ src/pages/log/books.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // import SEO from '../components/seo'
 // import styled from 'styled-components'
 import Page from '../../components/Page'
+import superagent from 'superagent'
 
-const BoogsLogPage = props => <Page {...props}><h1>Books</h1></Page>
+const BoogsLogPage = props => {
+    useEffect(() => {
+        superagent
+            .get('https://frozen-dusk-92791.herokuapp.com/goodreads/favorites')
+            .then(res => console.log(res))
+    }, [])
+
+    return (
+        <Page {...props}>
+            <h1>Books</h1>
+        </Page>
+    )
+}
 
 export default BoogsLogPage
