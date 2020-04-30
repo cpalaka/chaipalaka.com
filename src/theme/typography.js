bc3ba22582d12875fd72f134524bf3254f91e143
Chaitanya Palaka <cpalaka@gmail.com>
2020-04-29 22:46:31 -0700

    test some new typography themes

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -2,9 +2,13 @@ import Typography from 'typography'
 import wikipediaTheme from 'typography-theme-Wikipedia'
 import stAnnesTheme from 'typography-theme-st-annes'
 import twinPeaksTheme from 'typography-theme-twin-peaks'
+import stowLakeTheme from 'typography-theme-stow-lake' // **
+import oceanBeachTheme from 'typography-theme-ocean-beach' // **
+import irvingTheme from 'typography-theme-irving'
+import fairyGatesTheme from 'typography-theme-fairy-gates' // **
 
-stAnnesTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-    'h1, h2, h3, h4, h5, h6, p': {
+stowLakeTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+    'h1, h2, h3, h4, h5, h6, p': { //for st annes
         marginTop: rhythm(0),
         marginBottom: rhythm(0),
     },
@@ -18,6 +22,6 @@ stAnnesTheme.overrideThemeStyles = ({ rhythm }, options) => ({
 
 })
 
-const typography = new Typography(stAnnesTheme)
+const typography = new Typography(stowLakeTheme)
 
 export default typography
