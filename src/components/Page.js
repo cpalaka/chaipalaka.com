bbe66c55e4bf80a2ff1f2e83d6c2311b7a99e795
Chaitanya Palaka <cpalaka@gmail.com>
2020-05-10 20:01:46 -0700

    change right border to white

--- src/components/Page.js
+++ src/components/Page.js
@@ -13,7 +13,11 @@ export const PageSection = styled.div`
     padding-left: ${({ padding }) => (padding ? padding : '10px')};
     padding-right: ${({ padding }) => (padding ? padding : '10px')};
 
-    border: ${({theme}) => `3px solid ${theme.commonColors.borderBlack}`};
+    border-top: ${({theme}) => `3px solid ${theme.commonColors.borderBlack}`};
+    border-bottom: ${({theme}) => `3px solid ${theme.commonColors.borderBlack}`};
+    border-left: ${({theme}) => `3px solid ${theme.commonColors.borderBlack}`};
+    border-right: ${({theme}) => `3px solid white`};
+
     width: ${({ width }) => (width ? width + 'vw' : 'initial')};
 `
 
